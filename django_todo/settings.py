62c5a5fb2f7bc2ce8628d2ac45e1ed005c741522
vfg-95 <sam.ridding@hotmail.co.uk>
2022-04-20 17:48:33 +0000

    Render data

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from os import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,9 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from os import path
+
+
 if path.exists("env.py"):
-  import env
+    import env
+
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
--- django_todo/urls.py
+++ django_todo/urls.py
@@ -19,5 +19,5 @@ from todo.views import get_todo_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_todo_list, name = 'get_todo_list')
+    path('', get_todo_list, name='get_todo_list')
 ]
